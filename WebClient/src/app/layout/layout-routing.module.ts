9c1cacef10fc1b8c5554e11a25373ceba9bcc211
Sérgio Filho <s.filho@intcom.com.br>
2020-02-07 14:57:56 -0300

    removendo código não utilizado do template

--- WebClient/src/app/layout/layout-routing.module.ts
+++ WebClient/src/app/layout/layout-routing.module.ts
@@ -8,15 +8,9 @@ const routes: Routes = [
         component: LayoutComponent,
         children: [
             { path: '', redirectTo: 'list', pathMatch: 'prefix' },
-            { path: 'list', loadChildren: () => import('./todo-list/todo-list.module').then(m => m.ToDoListModule) },
-            { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
-            // { path: 'charts', loadChildren: () => import('./charts/charts.module').then(m => m.ChartsModule) },
-            // { path: 'tables', loadChildren: () => import('./tables/tables.module').then(m => m.TablesModule) },
-            // { path: 'forms', loadChildren: () => import('./form/form.module').then(m => m.FormModule) },
-            // { path: 'bs-element', loadChildren: () => import('./bs-element/bs-element.module').then(m => m.BsElementModule) },
-            // { path: 'grid', loadChildren: () => import('./grid/grid.module').then(m => m.GridModule) },
-            // { path: 'components', loadChildren: () => import('./bs-component/bs-component.module').then(m => m.BsComponentModule) },
-            // { path: 'blank-page', loadChildren: () => import('./blank-page/blank-page.module').then(m => m.BlankPageModule) }
+            { path: 'list', loadChildren: () => import('./todo-list/todo-list.module').then(m => m.ToDoListModule) }
+            //{ path: 'categories', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }
+            
         ]
     }
 ];
